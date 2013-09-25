53e37ee8eaebf791e7c72fd950a7d01e6dc0a05a
natasamajstorovic <natasa229@gmail.com>
2013-09-25 16:17:30 +0200

    formatirano logovanje

--- app/views/logovanjeBlade.blade.php
+++ app/views/logovanjeBlade.blade.php
@@ -8,47 +8,40 @@
 </head>
 
 <body>
+
 	
-	<div class="header" id="header">
-		<h1 class="navbar-inner">HEADER</h1>
-	</div>
 	
 	<div class="wrapper navbar navbar-inner">
-		<h1 class="underlined">LOGOVANJE</h1>
-		
-		<p class="serif">Unesite podatke</p>
-		{{ Form::open(array(
-				 'route'=>'zavrseno',
-				 'method' => 'POST',
-		)) }}
-		{{ Form::label('usernamelbl', Lang::get('logovanje.username')) }}<br>
-		{{ Form::text('username', '') }}<br>
-		{{ Form::label('passwordlbl', Lang::get('logovanje.password')) }}<br>
-		{{ Form::password('password') }}<br>
-		{{ Form::submit( Lang::get('logovanje.login')) }}
-		{{ Form::close() }}
-		 <div id='login-box' class='pull-right control-group'>
-		 <a href="{{ URL::to('registracija') }}">       
-		          REGISTRACIJA      
-		</a>
-		 </div>
-		<br>
-		<a href="{{ URL::to('login/en') }}">       
-		          english      
-		</a> <br>
-		<a href="{{ URL::to('login/srb') }}">      
-		          srpski    
-		</a>
-		 <div class="push"></div>
+	
+			{{ Form::open(array(
+					 'route'=>'zavrseno',
+					 'method' => 'POST',
+			)) }}
+			<div class="header" id="header">
+				<h1 class="navbar-inner">HEADER</h1></div>
+			<h1 class="underlined">{{Form::label('LOGOVANJE')}}</h1>
+			<div class="row">
+				<div class="span2"> <p class="serif">{{Form::label('Unesite podatke')}}</p></div>
+				<div class="span2 offset10"> <a href="{{ URL::to('registracija') }}"> REGISTRACIJA </a>   </div>      		            			
+				<div class="span1">	<a  href="{{ URL::to('login/en') }}"> english </a> </div>
+				<div class="sapn1">	<a  href="{{ URL::to('login/srb') }}"> srpski </a></div>
+			</div>
+			{{ Form::label('usernamelbl', Lang::get('logovanje.username')) }}<br>
+			{{ Form::text('username', '') }}<br>
+			{{ Form::label('passwordlbl', Lang::get('logovanje.password')) }}<br>
+			{{ Form::password('password') }}<br>
+			{{ Form::submit( Lang::get('logovanje.login')) }}
+			{{ Form::close() }}
+			 		
+			 <div class="push"></div>
 	</div> 
 	
 		
 	<div class="footer navbar-inner">
 		<li class="nav-header">FOOTER</li>
-   		 <li class="active"><a href="#header">pocetak</a></li></div>
+   		<li class="active"><a href="#header">pocetak</a></li></div>
     			   
     		
-	
 	
 
 </body>
